ec9e50042b042a4c46ee26bedba411a0208d6cc3
dymafr <erwan1309@hotmail.com>
2021-01-04 17:46:38 +0100

    1 file modified

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -6,6 +6,6 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class FilterPipe implements PipeTransform {
   transform(value: string[], search: string): string[] {
-    return value.filter(val => val.includes(search));
+    return value.filter(v => v.toLowerCase().includes(search));
   }
 }
