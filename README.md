05ea42311c71ff0add02c21f92d2da6150541906
dymafr <erwan1309@hotmail.com>
2021-01-04 17:16:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-chap14-l4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-chap14-l4)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { LOCALE_ID, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import localeFr from "@angular/common/locales/fr";
+import { registerLocaleData } from "@angular/common";
+import { CbPipe } from "./cb.pipe";
+registerLocaleData(localeFr);
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, CbPipe],
+  bootstrap: [AppComponent],
+  providers: [{ provide: LOCALE_ID, useValue: "fr-FR" }]
+})
+export class AppModule {}
