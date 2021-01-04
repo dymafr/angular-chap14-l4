dc6ff4a89ef3250638fce4c19e67e966c435238f
dymafr <erwan1309@hotmail.com>
2021-01-04 17:45:55 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from "./app.component";
 import localeFr from "@angular/common/locales/fr";
 import { registerLocaleData } from "@angular/common";
 import { CbPipe } from "./cb.pipe";
+import { FilterPipe } from './filter.pipe';
 registerLocaleData(localeFr);
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, CbPipe],
+  declarations: [AppComponent, CbPipe, FilterPipe],
   bootstrap: [AppComponent],
   providers: [{ provide: LOCALE_ID, useValue: "fr-FR" }]
 })
--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,12 +1,11 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'filter'
+  name: "filter",
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
-
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: string[], search: string): string[] {
+    return value.filter(val => val.includes(search));
   }
-
 }
